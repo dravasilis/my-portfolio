9ff5d73b352fabd65294f002cc519c81eef7e53f
dravasilis <dravasilis@gmail.com>
2024-07-05 16:36:39 +0300

    ✓set max height for vertical bar 2

--- src/components/experience/experience.tsx
+++ src/components/experience/experience.tsx
@@ -1,72 +1,94 @@
-import { useEffect, useRef } from 'react';
-import './experience.css'
-import { Timeline } from '../../models/timeline';
-import { log } from 'console';
+import { useEffect, useRef } from "react";
+import "./experience.css";
+import { Timeline } from "../../models/timeline";
+import { log } from "console";
 
+function Experience() {
+  const timestamps: Timeline[] = [
+    {
+      description:
+        "Started studying at Department of Information and Electronic Engineering",
+    },
+    {
+      description: "Started developing personal projects",
+    },
+    {
+      description:
+        "Started working at SmartUp IKE - a software company that develops applications",
+    },
+  ];
+  const timestampRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const yearLabelRef = useRef<HTMLDivElement>(null);
+  const yearLabelRef2 = useRef<HTMLDivElement>(null);
+  const yearLabelRef3 = useRef<HTMLDivElement>(null);
 
-function Experience( ){
-   const timestamps:Timeline[] =[
-      {
-         description:'Started studying at Department of Information and Electronic Engineering'
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            console.log(1);
+
+            entry.target.classList.add("visibleTimestamp");
+          } else {
+            entry.target.classList.remove("visibleTimestamp");
+          }
+        });
       },
       {
-         description:'Started developing personal projects'
-      },
-      {
-         description:'Started working at SmartUp IKE - a company that develops apps and admin platforms'
-      },
-      
-   ]
-   const timestampRefs = useRef<(HTMLDivElement | null)[]>([]);
-   const yearLabelRef = useRef<HTMLDivElement>(null)
-   const yearLabelRef2 = useRef<HTMLDivElement>(null)
-   const yearLabelRef3 = useRef<HTMLDivElement>(null)
+        threshold: 1,
+      }
+    );
 
-   useEffect(() => {
-      const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                 console.log(1);
-                 
-                  entry.target.classList.add('visibleTimestamp');
-              } 
-              else {
-                  entry.target.classList.remove('visibleTimestamp');
-              }
-          });
-      }, {
-          threshold: 1,
-      });
-
-      timestampRefs.current.forEach(ref => {
-         if (ref) {
-             observer.observe(ref);
-         }
-     });
-     if(yearLabelRef.current)
-         observer.observe(yearLabelRef.current)
-      if(yearLabelRef2.current)
-         observer.observe(yearLabelRef2.current)
-      if(yearLabelRef3.current)
-         observer.observe(yearLabelRef3.current)
-}, []);
-   return(
-   <div className=' flex flex-col '>
-      <span className='text-5xl text-teal-200 font-bold neon-sign-purple w-max rounded-xl p-4'> &lt; Experience / &gt;</span>
-      <div className=' w-max  ml-[36rem] top-[3800px] absolute  flex flex-col gap-[20rem]'>
-         <span ref={yearLabelRef} className='neon-sign-purple opacity-0    text-white w-max py-4 px-12 duration-1000 z-[1] text-3xl bg-slate-900  rounded-xl'>2018</span>
-         <span ref={yearLabelRef2} className='neon-sign-purple opacity-0   text-white w-max py-4 px-12 duration-1000 z-[1] text-3xl bg-slate-900  rounded-xl'>2021</span>
-         <span ref={yearLabelRef3} className='neon-sign-purple opacity-0   text-white w-max py-4 px-12 duration-1000 z-[1] text-3xl bg-slate-900  rounded-xl'>2023</span>
+    timestampRefs.current.forEach((ref) => {
+      if (ref) {
+        observer.observe(ref);
+      }
+    });
+    if (yearLabelRef.current) observer.observe(yearLabelRef.current);
+    if (yearLabelRef2.current) observer.observe(yearLabelRef2.current);
+    if (yearLabelRef3.current) observer.observe(yearLabelRef3.current);
+  }, []);
+  return (
+    <div className=" flex flex-col ">
+      <span className="text-5xl text-teal-200 font-bold neon-sign-purple w-max rounded-xl p-4">
+        {" "}
+        &lt; Experience / &gt;
+      </span>
+      <div className=" w-max  ml-[36rem] top-[3800px] absolute  flex flex-col gap-[20rem]">
+        <span
+          ref={yearLabelRef}
+          className="neon-sign-purple opacity-0    text-white w-max py-4 px-12 duration-1000 z-[1] text-3xl bg-slate-900  rounded-xl"
+        >
+          2018
+        </span>
+        <span
+          ref={yearLabelRef2}
+          className="neon-sign-purple opacity-0   text-white w-max py-4 px-12 duration-1000 z-[1] text-3xl bg-slate-900  rounded-xl"
+        >
+          2021
+        </span>
+        <span
+          ref={yearLabelRef3}
+          className="neon-sign-purple opacity-0   text-white w-max py-4 px-12 duration-1000 z-[1] text-3xl bg-slate-900  rounded-xl"
+        >
+          2023
+        </span>
       </div>
-      <div className=' flex flex-col gap-[20rem] py-[20rem]'>
-         {timestamps.map((timestamp,index)=>(
-            <div key={index} ref={el => timestampRefs.current[index] = el} 
-            className={`flex ${index===(1 || 3)?'relative -right-[32rem]':''} bg-slate-900 z-[1] flex-col gap-2 items-center neon-sign-blue -ml-44 duration-1000 opacity-0 p-8 w-max rounded-2xl `}>
-               <span className='text-white text-xl'>{timestamp.description}</span>
-            </div>
-         ))}
+      <div className=" flex flex-col gap-[20rem] py-[20rem]">
+        {timestamps.map((timestamp, index) => (
+          <div
+            key={index}
+            ref={(el) => (timestampRefs.current[index] = el)}
+            className={`flex ${
+              index === (1 || 3) ? "relative -right-[32rem]" : ""
+            } bg-slate-900 z-[1] flex-col gap-2 items-center neon-sign-blue -ml-44 duration-1000 opacity-0 p-8 w-max rounded-2xl `}
+          >
+            <span className="text-white text-xl">{timestamp.description}</span>
+          </div>
+        ))}
       </div>
-  </div> 
-   ) 
+    </div>
+  );
 }
 export default Experience;
--- src/components/main-view/Main-View.tsx
+++ src/components/main-view/Main-View.tsx
@@ -1,110 +1,145 @@
-import { useEffect, useRef, useState } from 'react';
-import './Main-View.css'
-import Navigation from '../navigation/navigation';
-import { BrowserRouter, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
-import Home from '../introduction/introduction';
-import Introduction from '../introduction/introduction';
-import Work from '../work/work';
-import { log } from 'console';
-import Experience from '../experience/experience';
-import { verify } from 'crypto';
-import AboutMe from '../about-me/about-me';
+import { useEffect, useRef, useState } from "react";
+import "./Main-View.css";
+import Navigation from "../navigation/navigation";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import Home from "../introduction/introduction";
+import Introduction from "../introduction/introduction";
+import Work from "../work/work";
+import { log } from "console";
+import Experience from "../experience/experience";
+import { verify } from "crypto";
+import AboutMe from "../about-me/about-me";
 
+function MainView() {
+  const starsRef = useRef<HTMLImageElement>(null);
+  const starsRef2 = useRef<HTMLImageElement>(null);
+  const routerLinkRef = useRef<HTMLDivElement>(null);
+  const verticalProgressbarRef = useRef<HTMLDivElement>(null);
+  const verticalProgressbarRef2 = useRef<HTMLDivElement>(null);
+  const experienceRef = useRef<HTMLDivElement>(null);
+  const horizontalProgressbarRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State lifted to parent
+  const [scrollToWork, setScrollToWork] = useState(false); // State lifted to parent
+  useEffect(() => {
+    if (!starsRef.current || !starsRef2.current || !routerLinkRef.current) {
+      return;
+    }
+    starsRef.current.style.transitionDuration = "1000ms";
+    starsRef2.current.style.transitionDuration = "2500ms";
+    routerLinkRef.current.style.transitionDuration = "2500ms";
+    setInterval(() => {
+      if (!starsRef.current || !routerLinkRef.current) {
+        return;
+      }
+      routerLinkRef.current.style.bottom = "30rem";
+      starsRef.current.style.opacity = "0.7";
+      routerLinkRef.current.style.opacity = "1";
+      starsRef.current.style.marginLeft = "-100px";
 
-
-function MainView(){
-    const starsRef = useRef<HTMLImageElement>(null)
-    const starsRef2 = useRef<HTMLImageElement>(null)
-    const routerLinkRef = useRef<HTMLDivElement>(null)
-    const verticalProgressbarRef = useRef<HTMLDivElement>(null)
-    const verticalProgressbarRef2 = useRef<HTMLDivElement>(null)
-    const experienceRef = useRef<HTMLDivElement>(null)
-    const horizontalProgressbarRef = useRef<HTMLDivElement>(null)
-    const [isMenuOpen, setIsMenuOpen] = useState(false); // State lifted to parent
-    const [scrollToWork, setScrollToWork] = useState(false); // State lifted to parent
-    useEffect(() => {
-        if(!starsRef.current || !starsRef2.current || !routerLinkRef.current   ){return}
-            starsRef.current.style.transitionDuration = '1000ms';
-            starsRef2.current.style.transitionDuration = '2500ms';
-            routerLinkRef.current.style.transitionDuration = '2500ms';
-            setInterval(()=>{
-                if(!starsRef.current ||  !routerLinkRef.current   ){return}
-                routerLinkRef.current.style.bottom = '30rem';
-                starsRef.current.style.opacity = '0.7'
-                routerLinkRef.current.style.opacity = '1'
-                starsRef.current.style.marginLeft = '-100px'
-                
-                setInterval(()=>{
-                if(!starsRef2.current  ){return}
-                    starsRef2.current.style.opacity = '0.3'
-                    starsRef2.current.style.marginLeft = '0px'
-                },100)
-            },1000)
-
-      },[]);
-
-     
-
-      window.addEventListener('scroll', ()=>{
-        const scrollValue = document.documentElement.scrollTop;
-        const maxVerticalHeight = 3700; // Maximum height for the vertical bar
-        const maxHorizontalWidth = 720; // 45rem in pixels
-        const verticalThresholdScroll = maxVerticalHeight / 1.2;
-      const horizontalThresholdScroll = verticalThresholdScroll + (maxHorizontalWidth / 2);
-        if (scrollValue <= verticalThresholdScroll ) {
-            if( verticalProgressbarRef.current && horizontalProgressbarRef.current && verticalProgressbarRef2.current)   {
-                verticalProgressbarRef.current.style.height = (scrollValue * 1.2) + 'px';
-                horizontalProgressbarRef.current.style.width = '0px';
-                verticalProgressbarRef2.current.style.height = '0px';
-            }
-          } 
-        else if(scrollValue <= horizontalThresholdScroll) {
-           if( verticalProgressbarRef.current && horizontalProgressbarRef.current  && verticalProgressbarRef2.current){
-               verticalProgressbarRef.current.style.height = maxVerticalHeight + 'px';
-               horizontalProgressbarRef.current.style.width = ((scrollValue - verticalThresholdScroll) * 1.9) + 'px';
-               verticalProgressbarRef2.current.style.height = '0px';
-           }
+      setInterval(() => {
+        if (!starsRef2.current) {
+          return;
         }
-        else{
-            if( verticalProgressbarRef.current && horizontalProgressbarRef.current  && verticalProgressbarRef2.current){
-                verticalProgressbarRef.current.style.height = maxVerticalHeight + 'px';
-                horizontalProgressbarRef.current.style.width = maxHorizontalWidth + 'px';
-                verticalProgressbarRef2.current.style.height = ((scrollValue - horizontalThresholdScroll) * 1.5) + 'px';
-            }
-          }
+        starsRef2.current.style.opacity = "0.3";
+        starsRef2.current.style.marginLeft = "0px";
+      }, 100);
+    }, 1000);
+  }, []);
 
-        
-      })
-     
-    return(
-        <div className="view h-full"  >
-            <div className=' w-full fixed top-0 z-10'  >
-                <Navigation isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
-            </div>
-            <div className="image-container">
-                <div ref={starsRef} className='image'  ></div>
-                <img ref={starsRef2} src="/assets/img/stars.jpg" alt="stars" className=' opacity-0 object-cover absolute   top-0 h-[50rem] w-full'/>
-            </div>  
-            <div ref={routerLinkRef}   className="gap-8 opacity-0 relative bottom-[33rem]" >
-                 <div ref={verticalProgressbarRef} className='min-h-[25rem] max-h-[3700px]   absolute ml-80  p-[2px] z-0 rounded-3xl bg-myTeal glow'></div>
-                 <div ref={horizontalProgressbarRef} className='absolute max-w-[45rem]    ml-80 top-[3700px]  p-[2px] z-0 rounded-3xl bg-myTeal glow'></div>
-                 <div ref={verticalProgressbarRef2} className='absolute    ml-[65rem] top-[3700px]  p-[2px] z-0 rounded-3xl bg-myTeal glow'></div>
-                 <div className='flex flex-col pl-96 gap-96' >
-                    <Introduction setScrollToWork={setScrollToWork}/>
-                    <div id='about-me'>
-                        <AboutMe />
-                    </div>
-                    <div id='work'>
-                        <Work />
-                    </div>
-                    <div id='experience' ref={experienceRef}>
-                        <Experience   />
-                    </div>
-                 </div>
-            </div>
-        
+  window.addEventListener("scroll", () => {
+    const scrollValue = document.documentElement.scrollTop;
+    const maxVerticalHeight = 3700; // Maximum height for the vertical bar
+    const maxHorizontalWidth = 720; // 45rem in pixels
+    const verticalThresholdScroll = maxVerticalHeight / 1.2;
+    const horizontalThresholdScroll =
+      verticalThresholdScroll + maxHorizontalWidth / 2;
+    if (scrollValue <= verticalThresholdScroll) {
+      if (
+        verticalProgressbarRef.current &&
+        horizontalProgressbarRef.current &&
+        verticalProgressbarRef2.current
+      ) {
+        verticalProgressbarRef.current.style.height = scrollValue * 1.2 + "px";
+        horizontalProgressbarRef.current.style.width = "0px";
+        verticalProgressbarRef2.current.style.height = "0px";
+      }
+    } else if (scrollValue <= horizontalThresholdScroll) {
+      if (
+        verticalProgressbarRef.current &&
+        horizontalProgressbarRef.current &&
+        verticalProgressbarRef2.current
+      ) {
+        verticalProgressbarRef.current.style.height = maxVerticalHeight + "px";
+        horizontalProgressbarRef.current.style.width =
+          (scrollValue - verticalThresholdScroll) * 1.9 + "px";
+        verticalProgressbarRef2.current.style.height = "0px";
+      }
+    } else {
+      if (
+        verticalProgressbarRef.current &&
+        horizontalProgressbarRef.current &&
+        verticalProgressbarRef2.current
+      ) {
+        verticalProgressbarRef.current.style.height = maxVerticalHeight + "px";
+        horizontalProgressbarRef.current.style.width =
+          maxHorizontalWidth + "px";
+        verticalProgressbarRef2.current.style.height =
+          (scrollValue - horizontalThresholdScroll) * 1.5 + "px";
+      }
+    }
+  });
+
+  return (
+    <div className="view h-full">
+      <div className=" w-full fixed top-0 z-10">
+        <Navigation isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      </div>
+      <div className="image-container">
+        <div ref={starsRef} className="image"></div>
+        <img
+          ref={starsRef2}
+          src="/assets/img/stars.jpg"
+          alt="stars"
+          className=" opacity-0 object-cover absolute   top-0 h-[50rem] w-full"
+        />
+      </div>
+      <div
+        ref={routerLinkRef}
+        className="gap-8 opacity-0 relative bottom-[33rem]"
+      >
+        <div
+          ref={verticalProgressbarRef}
+          className="min-h-[25rem] max-h-[3700px]   absolute ml-80  p-[2px] z-0 rounded-3xl bg-myTeal glow"
+        ></div>
+        <div
+          ref={horizontalProgressbarRef}
+          className="absolute max-w-[45rem]    ml-80 top-[3700px]  p-[2px] z-0 rounded-3xl bg-myTeal glow"
+        ></div>
+        <div
+          ref={verticalProgressbarRef2}
+          className="absolute max-h-[1500px]   ml-[65rem] top-[3700px]  p-[2px] z-0 rounded-3xl bg-myTeal glow"
+        ></div>
+        <div className="flex flex-col pl-96 gap-96">
+          <Introduction setScrollToWork={setScrollToWork} />
+          <div id="about-me">
+            <AboutMe />
           </div>
-    )
+          <div id="work">
+            <Work />
+          </div>
+          <div id="experience" ref={experienceRef}>
+            <Experience />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainView;
